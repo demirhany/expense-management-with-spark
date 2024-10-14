e2e7345c93c9e5d9db08896ccca0764541dad897
Demirhan Yilal <demirhanyilal0@gmail.com>
2024-10-14 16:23:25 +0300

    initial commit

---
+++ big_data_hw_spring/src/main/java/org/ozo/big_data_hw_spring/BigDataHwSpringApplication.java
@@ -0,0 +1,13 @@
+package org.ozo.big_data_hw_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataHwSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigDataHwSpringApplication.class, args);
+    }
+
+}
---
+++ big_data_hw_spring/src/test/java/org/ozo/big_data_hw_spring/BigDataHwSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ozo.big_data_hw_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataHwSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
