fe9bafedff29931fd0fdcff63975479d64b9936f
demirhany <demirhanyilal0@gmail.com>
2024-11-02 16:17:44 +0300

    hadoop hw: initial commit

---
+++ big-data-hadoop-spring/src/main/java/com/ozo/bigdatahadoopspring/BigDataHadoopSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ozo.bigdatahadoopspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataHadoopSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDataHadoopSpringApplication.class, args);
+	}
+
+}
---
+++ big-data-hadoop-spring/src/test/java/com/ozo/bigdatahadoopspring/BigDataHadoopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozo.bigdatahadoopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataHadoopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
