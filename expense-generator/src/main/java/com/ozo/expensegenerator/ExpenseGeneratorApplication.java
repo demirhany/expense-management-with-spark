7d74dddd13f783421849a572b055f310fe2b7b5e
demirhany <demirhanyilal0@gmail.com>
2024-12-04 14:56:16 +0300

    initial commit for expense-generator

---
+++ expense-generator/src/main/java/com/ozo/expensegenerator/ExpenseGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ozo.expensegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseGeneratorApplication.class, args);
+	}
+
+}
---
+++ expense-generator/src/test/java/com/ozo/expensegenerator/ExpenseGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozo.expensegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
